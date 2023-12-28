cdf819ea0e34f16b9d2b28bb5c9db618de40cc28
Liam <liamsuu14974@gmail.com>
2023-12-28 17:57:25 +0000

    Complete assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let wordArr = [];
+  for (let x = 0; x < word.length; x++) {
+    wordArr.push(word.charAt(x));
+  }
+  wordArr = wordArr.reverse();
+  return wordArr.join("");
 };
 
 // Do not edit below this line
