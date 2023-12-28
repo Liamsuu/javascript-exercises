db303f356767a736b1055648e2cd218e1028c371
Liam <liamsuu14974@gmail.com>
2023-12-28 18:30:54 +0000

    Complete assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arrayValues, ...args) {
+  for (let x = 0; x < args.length; x++) {
+    if (arrayValues.includes(args[x])) {
+      let valueIndex = arrayValues.indexOf(args[x]);
+      arrayValues.splice(valueIndex, 1);
+    }
+  }
+  return arrayValues;
 };
 
 // Do not edit below this line
