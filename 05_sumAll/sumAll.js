9a9b552e757b7623e6c75506709cb64147855de9
Liam <liamsuu14974@gmail.com>
2023-12-28 19:47:02 +0000

    Complete assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  let sum = 0;
+  // change this after food i believe it is causing error
+  if (typeof numOne !== "number" || typeof numTwo !== "number") {
+    return "ERROR";
+  } else if (numOne < 0 || numTwo < 0) {
+    return "ERROR";
+  }
 
+  if (numOne < numTwo) {
+    for (numOne; numOne !== numTwo + 1; numOne++) {
+      sum += numOne;
+    }
+  } else {
+    for (numTwo; numTwo !== numOne + 1; numTwo++) {
+      sum += numTwo;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
