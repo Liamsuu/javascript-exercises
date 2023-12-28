e2acf980db4059360cabc2009806b17d3781aa9d
Liam <liamsuu14974@gmail.com>
2023-12-28 20:50:27 +0000

    Complete assignment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature_f) {
+  return Math.round((((temperature_f - 32) * 5) / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature_c) {
+  return Math.round((temperature_c * 1.8 + 32) * 10) / 10;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
