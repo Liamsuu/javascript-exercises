9e6ed69f8c17b85f9d31ada24c5ea91a3ba0185a
Liam <liamsuu14974@gmail.com>
2023-12-28 16:46:15 +0000

    Complete assignment 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeats) {
+  const wordArr = [];
+  if (repeats < 0) {
+    return "ERROR";
+  }
+  for (let x = 0; x < repeats; x++) {
+    wordArr.push(word);
+  }
+  word = wordArr.join("");
+  return word;
 };
 
 // Do not edit below this line
