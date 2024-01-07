4066f998e0666a4766ead24885660612a7648e4b
Liam <liamsuu14974@gmail.com>
2024-01-07 19:05:27 +0000

    Complete assignment 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  // removes difficult characters from original sentence
+  word = word.split("");
+  let wordWithoutPunc = [];
+  for (const item of word) {
+    if (item !== " " && item !== "!" && item !== "." && item !== ",") {
+      wordWithoutPunc.push(item.toLowerCase());
+    }
+  }
+  let reversedWord = wordWithoutPunc.slice().reverse();
+  reversedWord = reversedWord.toString();
+  wordWithoutPunc = wordWithoutPunc.toString();
 
+  if (wordWithoutPunc == reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
